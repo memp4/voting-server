6dd11a7de705c97b917c24087614be7eb5a01fa6
Sultan <godsultan42@gmail.com>
2022-06-29 14:26:26 +0200

    feature: getWinners() func used in next()

--- src/core.ts
+++ src/core.ts
@@ -2,19 +2,30 @@ import Immutable, { Map, List, Collection } from 'immutable';
 
 // Define type for Redux store branches?
 // interface ReduxStore = {}
+type AnyMap = Map<string, any>
 
-export function setEntries(state: Map<string, any>, entries: string[]): Map<string, any> {
+export function setEntries(state: AnyMap, entries: string[]): AnyMap {
     return state.set('entries', List(entries));
 }
 
-export function next(state: Map<string, any>): Map<string, any> {
-    const entries = state.get('entries');
+function getWinners(vote: AnyMap): string[] {
+    if (!vote) return [];
+    const [a, b] = vote.get('pair');
+    const aVotes = vote.getIn(['tally', a], 0) as number;
+    const bVotes = vote.getIn(['tally', b], 0) as number;
+    if (aVotes > bVotes) return [a];
+    else if (aVotes < bVotes) return [b];
+    else return [a, b];
+}
+
+export function next(state: AnyMap): AnyMap {
+    const entries = state.get('entries').concat(getWinners(state.get('vote')));
     return state.merge({
         vote: Map({ pair: entries.take(2) }),
         entries: entries.skip(2) //returns List with undefined values
     });
 }
 
-export function vote(state: Map<string, any>, entry: string): Map<string, any> {
+export function vote(state: AnyMap, entry: string): AnyMap {
     return state.updateIn(['vote', 'tally', entry], 0, (tally) => tally as number + 1);
 }
