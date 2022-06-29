3027ef8a24576301d188a4e86d318bd2cbae9318
Sultan <godsultan42@gmail.com>
2022-06-29 14:44:48 +0200

    feature: next() return winner

--- src/core.ts
+++ src/core.ts
@@ -20,10 +20,16 @@ function getWinners(vote: AnyMap): string[] {
 
 export function next(state: AnyMap): AnyMap {
     const entries = state.get('entries').concat(getWinners(state.get('vote')));
-    return state.merge({
-        vote: Map({ pair: entries.take(2) }),
-        entries: entries.skip(2) //returns List with undefined values
-    });
+    if (entries.size === 1) {
+        return state.remove('vote')
+            .remove('entries')
+            .set('winner', entries.first());
+    } else {
+        return state.merge({
+            vote: Map({ pair: entries.take(2) }),
+            entries: entries.skip(2) //returns List with undefined values
+        });
+    }
 }
 
 export function vote(state: AnyMap, entry: string): AnyMap {
